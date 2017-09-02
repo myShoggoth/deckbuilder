997c3686fe47a9e7eaaf68be1f722f1fb90c5b3d
Andrew Boardman <andrew@brainbankers.com>
2017-09-02 13:45:14 -0700

    Calculate the game result, show results of lots of games by who one how many and how many ties

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,11 +12,12 @@ module Lib
     , testDominionTurn
     , testPlayerFind
     , testDominionDeals
+    , testDominionGames
     ) where
 
 import System.Random
 import System.Random.Shuffle
-import Data.List (delete, find, sortBy)
+import Data.List (delete, find, sortBy, group, sort, groupBy)
 import qualified Data.Map as Map
 
 -- Playing Cards
@@ -81,6 +82,11 @@ data Player = Player {
 instance Eq Player where
   a == b = _playerName a == _playerName b
 
+instance Ord Player where
+  compare p1 p2 = compare (_playerName p1) (_playerName p2)
+
+type Result = Either String Int
+
 treasureCardAction :: Int -> Card -> Player -> GameState -> GameState
 treasureCardAction amt c p gs = changeTurn player gs
   where player  = Player (_playerName p') (_deck p') (c : _discard p') (delete c (_hand p')) (_actions p') (_buys p') (amt + (_money p')) (_victory p')
@@ -133,6 +139,16 @@ tallyAllPoints p gs = evaluateHand player $ changeTurn player gs
 sortByPoints :: GameState -> GameState
 sortByPoints gs = GameState (reverse (sortBy (\p1 p2 -> compare (_victory p1) (_victory p2)) (_players gs))) (_decks gs) (_random gs)
 
+gameResult' :: [Player] -> Result
+gameResult' players = result ((length . head) grouped) players
+  where grouped = groupBy (\p1 p2 -> (_victory p1) == (_victory p2)) players
+        result 1 l = Left $ _playerName $ head l
+        result n _ = Right n
+
+gameResult :: GameState -> Result
+gameResult gs = gameResult' (_players sorted)
+  where sorted = sortByPoints (foldr tallyAllPoints gs (_players gs))
+
 decreaseCards :: Card -> Card -> Int -> Int
 decreaseCards  _  _ 0 = 0
 decreaseCards c1 c2 n = if (c1 == c2)
@@ -170,24 +186,31 @@ resetTurn p gs = changeTurn player gs
   where Just p' = find (== p) (_players gs)
         player  = Player (_playerName p') (_deck p') (_discard p') (_hand p') 1 1 0 0
 
-doTurn :: Player -> GameState -> GameState
-doTurn p = (resetTurn p) . (deal 5 p) . (bigMoneyBuy p) . (evaluateHand p)
+doTurn :: Bool -> Player -> GameState -> GameState
+doTurn True  p gs = gs
+doTurn False p gs = ( (resetTurn p) . (deal 5 p) . (bigMoneyBuy p) . (evaluateHand p) ) gs
 
 isGameOver :: GameState -> Bool
 isGameOver gs = ((_decks gs) Map.! provinceCard == 0) || numEmptyDecks >= 3
   where numEmptyDecks = length $ Map.filter (== 0) (_decks gs)
 
 runGame' :: Bool -> GameState -> GameState
-runGame' True gs  = gs
+runGame' True  gs = sortByPoints (foldr tallyAllPoints gs (_players gs))
 runGame' False gs = runGame' (isGameOver gs') gs'
-  where gs' = foldr doTurn gs (_players gs)
+  where gs' = foldr (doTurn (isGameOver gs)) gs (_players gs)
 
-runGame :: [Player] -> IO (GameState)
+runGame :: [Player] -> IO (Result)
 runGame players = do
   g <- newStdGen
-  let gs' = runGame' False $ GameState players (basicDecks (length players)) g
-  let gs'' = foldr tallyAllPoints gs' (_players gs')
-  return $ sortByPoints gs''
+  return $ gameResult $ runGame' False $ GameState players (basicDecks (length players)) g
+
+runGames :: Int -> [Player] -> IO ([(Result, Int)])
+runGames num players = do
+  g <- newStdGen
+  let seeds = take num $ randoms g
+  let gens = map mkStdGen seeds
+  let gses = map (GameState players (basicDecks (length players))) gens
+  return $ map (\l -> (head l, length l)) $ group $ sort $ map (gameResult . (runGame' False)) gses
 
 -- Dominion Game testing functions
 
@@ -230,7 +253,7 @@ testDominionRound = do
   let players = [ newPlayer "Player 1", newPlayer "Player 2"]
   let gs = (GameState players (basicDecks (length players)) g)
   let gs' = foldr (deal 5) gs players
-  return $ foldr doTurn gs' players
+  return $ foldr (doTurn False) gs' players
 
 testDominionTurn :: IO (GameState)
 testDominionTurn = do
@@ -238,7 +261,7 @@ testDominionTurn = do
   let players = [ newPlayer "Player 1", newPlayer "Player 2"]
   let gs = (GameState players (basicDecks (length players)) g)
   let gs' = foldr (deal 5) gs players
-  return $ doTurn (head players) gs'
+  return $ doTurn False (head players) gs'
 
 testDominionDeals :: IO (GameState)
 testDominionDeals = do
@@ -247,10 +270,15 @@ testDominionDeals = do
   let gs = (GameState players (basicDecks (length players)) g)
   return $ foldr (deal 5) gs players
 
-testDominionGame :: IO (GameState)
+testDominionGame :: IO (Result)
 testDominionGame = do
   g <- newStdGen
   runGame [newPlayer "Player 1", newPlayer "Player 2"]
+
+testDominionGames :: IO ([(Result, Int)])
+testDominionGames = do
+  g <- newStdGen
+  runGames 1000 [newPlayer "Player 1", newPlayer "Player 2"]
 
 testPlayerFind :: IO (Player)
 testPlayerFind = do
